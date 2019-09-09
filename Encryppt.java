117cae452fc3d251406ccddf8582f05f0f73c96a
Bhumit Malvi <bhumitmalvi999@gmail.com>
2019-09-09 05:52:25 -0700

    convert Returns binary value in an Array

--- Encryppt.java
+++ Encryppt.java
@@ -3,30 +3,34 @@
 import java.util.Scanner;
 class Convert
 {
-    public void Binary(int n) 
+    public int[] Binary(int n)
     {
-        int count = 0, a;
+        int count=0,i=0,a,k;
+        int d[]=new int[8];
         String x = "";
-        
+        d[i]=0;
         while(n > 0)
         {
+            i++;
             a = n % 2;
             x = x + "" + a;
             n = n / 2;
+            d[i]=a;
         }
         System.out.println("Binary number:"+x);
-        //return(x);
+        return(d);
     }
 }
-class Encryppt 
+class Encryppt
 {
     public static void main(String arg[])
     {
         Scanner sc=new Scanner(System.in);
         Convert c= new Convert();
         String data;
-	int n,i,j;
+        int n,i,j;
         char ch;
+        int dat[]= new int[8];
         System.out.println("Enter the Character to be Encrypted");
         data=sc.nextLine();
         n=data.length();
@@ -35,8 +39,8 @@ class Encryppt
             ch=data.charAt(i);
             j=(int)ch;
             System.out.println(j);
-            c.Binary(j);
-        } 
+            dat=c.Binary(j);
+            System.out.println(dat[1]);
+        }
     }
 }
-    
